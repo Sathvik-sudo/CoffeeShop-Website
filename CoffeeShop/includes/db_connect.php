c74d2f75f397d1fb6b8a7122658f363395f28f9a
Sathvik-sudo <53989217+Sathvik-sudo@users.noreply.github.com>
2024-09-24 22:56:14 +0530

    Update db_connect.php

--- CoffeeShop/includes/db_connect.php
+++ CoffeeShop/includes/db_connect.php
@@ -1,12 +1,19 @@
 <?php 
-    $servername="localhost";
-    $username="root";
-    $password="";
-    $database="coffeeshop";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "coffeeshop";
 
-    $conn = mysqli_connect($servername,$username,$password,$database);
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
     
-    if(mysqli_connect_errno($conn))
-       die("Connection failed!".mysqli_connect_error());
-?>
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
+    // Check for connection errors
+    if (mysqli_connect_errno()) {
+        die("Failed to connect to MySQL: " . mysqli_connect_error());
+    }
+?>
